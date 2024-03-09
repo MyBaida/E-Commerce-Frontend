90d1bf60209c2b1ce3b1b86717dc675a71f8958c
Edward Acquah <eaacquah3002@gmail.com>
2024-03-09 17:01:00 +0000

    just the Home Page

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import products from '../products'
+import {Row, Col} from 'react-bootstrap'
+import Product from '../Components/Product'
+
+
+
+
+const HomeScreen = () => {
+  return (
+    <div>
+        <h1>Latest Products</h1>
+        <Row>
+            {products.map((product) => (
+                <Col key={product._id} sm={12} md={6} lg={4} xl={3} >
+                  <Product product={product}/>
+                </Col>
+            ))}
+        </Row>
+    </div>
+  )
+}
+
+export default HomeScreen
